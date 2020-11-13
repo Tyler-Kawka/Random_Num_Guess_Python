4b456400cd4ab16dde1fd7d7571316e2864f1559
Tyler Kawka <tylerkawka@gmail.com>
2020-11-13 16:02:29 -0600

    Bugs with Binary Search

--- Random_Num.py
+++ Random_Num.py
@@ -4,11 +4,13 @@ import sys
 
 seed(1)
 class Random_Num_Guess:
-    def __init__(self,min=0,max=100):
+    def __init__(self,min=0,max=100,toggle=0):
         self.guesses = 0
+        self.guess_count = 0
         self.number = n.random.randint(min,max)
         self.min = min
         self.max = max
+        self.toggle = toggle
     
     def get_guess(self):
         guess = input(f"Please enter your guess between " + str(self.min) + " and " + str(self.max) + ": " )
@@ -17,18 +19,43 @@ class Random_Num_Guess:
         else:
             print("That is not a valid guess, please try again")
             return self.get_guess()
+    
+    #Optional function that will run binary search and guess the number using algorithm
+    def guess_num_binary_search(self,start,end):
+
+        #double divide sign to floor the number
+        middle = (start + end)//2
+        #keep track of guesses
+        self.guess_count += 1
+        if (middle < self.number):
+            print("Guess " + str(self.guess_count) + " was: " + str(middle))
+            return self.guess_num_binary_search(middle+1,end)
+        elif (middle > self.number):
+            print("Guess " + str(self.guess_count) + " was: " + str(middle))
+            return self.guess_num_binary_search(start,middle-1)
+        else:
+            print("Guess " + str(self.guess_count) + " was: " + str(middle))
+            print("Number is " + str(self.number) + ", Binary Search took " +  str(self.guess_count) + " guesses!")
+            self.play_again()
+        
 
     def valid_guess(self,num):
         return self.min <= int(num) <= self.max
     
     def play_again (self):
-        again = input("Play again [Y,N]? Game will be run with random value range between 0-1000: ")
-        if again == "Y":
-            next_game = Random_Num_Guess(0,n.random.randint(1,1000))
-            next_game.play()
+        if self.toggle == 0:
+            again = input("Play again [Y,N]? Game will be run with random value range between 0-1,000: ")
+            if again == "Y":
+                next_game = Random_Num_Guess(0,n.random.randint(1,1000))
+                next_game.play()
+            else:
+                exit()
         else:
-            exit()
-
+            next_game = Random_Num_Guess(0,n.random.randint(1,1000))
+            print("Running Binary Search for number with range of " + str(next_game.min) + " " + str(next_game.max))
+            next_game.guess_num_binary_search(next_game.min,next_game.max)
+            
+       
     def play(self):
         while 1:
             self.guesses += 1
@@ -42,10 +69,14 @@ class Random_Num_Guess:
         print("Congratulations you have guessed the number!")
         print("It only took you " + str(self.guesses) + " tries!")
         self.play_again()
+    
      
 if (len(sys.argv) < 2):
     newgame = Random_Num_Guess()
     newgame.play()
+elif (len(sys.argv) == 4):
+    newgame = Random_Num_Guess(int(sys.argv[1]),int(sys.argv[2]),1)
+    newgame.guess_num_binary_search(newgame.min,newgame.max)
 else:
-    newgame = Random_Num_Guess(int(sys.argv[1]),int(sys.argv[2]))
+    newgame = newgame = Random_Num_Guess(int(sys.argv[1]),int(sys.argv[2]))
     newgame.play()
